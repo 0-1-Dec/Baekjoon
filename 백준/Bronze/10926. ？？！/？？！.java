843cd2d47be85b1c5b3c38d444b914ba499b2e8b
JunYeong Jang <120743960+0-1-Dec@users.noreply.github.com>
2024-11-22 11:24:48 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        String a;
+        Scanner scan = new Scanner(System.in);
+        a = scan.next();
+        System.out.print(a + "??!");
+    }
+}
