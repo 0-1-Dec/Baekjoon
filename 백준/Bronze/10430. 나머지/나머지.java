ef24abd9fab0bf86a0e76f9332ddfea0a7b6a082
JunYeong Jang <120743960+0-1-Dec@users.noreply.github.com>
2024-11-22 11:24:21 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        int a, b, c;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
